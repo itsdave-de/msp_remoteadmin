c026d95060025ce18206a51274027bd167a5d938
Luiz Costa <luizgustavo@luizgustavo.pro.br>
2024-06-03 14:41:15 +0100

    add domain parameters when exists with RDP

--- msp_remoteadmin/tools.py
+++ msp_remoteadmin/tools.py
@@ -35,12 +35,13 @@ def create_session(name, protocol):
             acc_doc = frappe.get_doc('IT User Account', doc.get('link'))
             username = acc_doc.username
             password = acc_doc.get_password('password')
+            domain = acc_doc.get('domain') if acc_doc.get('domain') else None
             # Get IP
             ip_doc = frappe.get_doc('IP Address', doc.get('main_ip'))
             ip_address = ip_doc.ip_address
             
-            uri = urllib.parse.quote_plus(f"{protocol}://{username if username else ''}{':' + password if password else ''}@{ip_address}{':' + str(PROTOCOL_PORT[protocol])}").lower()
+            uri = f"{protocol}://{username if username else ''}{':' + password if password else ''}@{ip_address}{':' + str(PROTOCOL_PORT[protocol])}"
             if protocol == 'RDP':
-                uri = f"{uri}/?ignore-cert=true&disable-audio=true"
-            url = f'{guaca_config.guacamole_server}/?#/?token={token}&quickconnect={uri}'
+                uri = f"{uri}/?ignore-cert=true&disable-audio=true{'&domain=' + domain if domain else ''}"
+            url = f'{guaca_config.guacamole_server}/?#/?token={token}&quickconnect={urllib.parse.quote_plus(uri).lower()}'
             return url
