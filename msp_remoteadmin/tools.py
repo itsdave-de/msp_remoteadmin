894335f2c5d8eb8cd18a71569d236e9a4066b1c7
Luiz Costa <luizgustavo@luizgustavo.pro.br>
2024-06-03 12:26:19 +0100

    debug

--- msp_remoteadmin/tools.py
+++ msp_remoteadmin/tools.py
@@ -32,8 +32,8 @@ def create_session(doc, protocol):
             print(f"Token: {token}")
             # Get credentials from IT User Account
             print(f"Values from form: {doc}")
-            print(f"Type: {type(doc.link)}")
-            acc_doc = frappe.get_doc('IT User Account', doc.link)
+            print(f"Type: {type(doc.get('link'))}")
+            acc_doc = frappe.get_doc('IT User Account', doc.get('link'))
             print(f"Print doc values: {acc_doc}")
             username = acc_doc.username
             password = acc_doc.get_password('password')
