81147fd9a66ae1f50e511bed493702a58da4681c
Luiz Costa <luizgustavo@luizgustavo.pro.br>
2024-06-05 15:28:44 +0200

    add new fields on report sessions

---
+++ msp_remoteadmin/msp_remoteadmin/doctype/remote_connection_sessions/remote_connection_sessions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Luiz Costa and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Remote Connection Sessions', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ msp_remoteadmin/msp_remoteadmin/doctype/remote_connection_sessions/test_remote_connection_sessions.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Luiz Costa and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRemoteConnectionSessions(FrappeTestCase):
+	pass
